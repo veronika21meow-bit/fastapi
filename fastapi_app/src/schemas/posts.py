9c5f3780c632b1690e65c8eee50971a5c32bbf6b
Popova Veronika <veronika21meow@gmail.com>
2026-02-26 08:58:32 +0400

    feat: update model posts

--- fastapi_app/src/schemas/posts.py
+++ fastapi_app/src/schemas/posts.py
@@ -1,1 +1,23 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
+from schemas.users import User
+from schemas.locations import Location
+from schemas.categories import Category
+
+
+
+class Post(BaseModel):
+    title: str = Field(..., description='Заголовок', max_length=256)
+    text: str = Field(..., description='Текст')
+    image: str = Field(description="Ссылка на изображение")
+    pup_date: datetime = Field(description=
+        'Дата публикации. Если установить дату и время '
+        'в будущем — можно делать отложенные публикации.'
+    )
+    author: User = Field(..., description='Автор публикации')
+    location: Location = Field(description='Местоположение')
+    category: Category = Field(description='Категория')
+    is_published: bool = Field(..., description='Опубликовано. Снимите галочку, чтобы скрыть публикацию.')
+    create_at: datetime = Field(..., description='Добавлено')
+
+
