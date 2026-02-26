a5a70cc130cf246203e9065c4570d2e7324bcd51
Popova Veronika <veronika21meow@gmail.com>
2026-02-26 09:20:44 +0400

    feat: add model location

---
+++ fastapi_app/src/schemas/locations.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class Location(BaseModel):
+    name: str = Field(..., description='Заголовок', max_length=256)
+    is_published: bool = Field(..., description='Опубликовано. Снимите галочку, чтобы скрыть публикацию.')
+    create_at: datetime = Field(..., description='Добавлено')
+
+
