3b2f582dfee5272fea0a4379245f51ea35829141
Popova Veronika <veronika21meow@gmail.com>
2026-03-13 01:45:16 +0400

    feat: add model category for db

---
+++ fastapi_app/src/infrastructure/sqlite/models/categories.py
@@ -0,0 +1,15 @@
+from infrastructure.sqlite.database import Base
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, unique=True)
+    title: Mapped[str] = mapped_column(String(256), nullable=False, unique=True)
+    description: Mapped[str] = mapped_column(nullable=False)
+    slug: Mapped[str] = mapped_column(nullable=False, unique=True)
+    is_published: Mapped[bool] = mapped_column(nullable=False)
+    create_at: Mapped[datetime] = mapped_column(nullable=False)
