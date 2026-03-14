eaf9c1fa65675730af43bfe26f457f17d76db6ea
Popova Veronika <veronika21meow@gmail.com>
2026-03-14 17:34:00 +0400

    feat: add post repositories

---
+++ fastapi_app/src/infrastructure/sqlite/repositories/posts.py
@@ -0,0 +1,76 @@
+from typing import Type, List
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+
+from infrastructure.sqlite.models.posts import Post
+
+
+class PostRepository:
+    def __init__(self):
+        self._model: Type[Post] = Post
+
+    def get_post_by_id(self, session: Session, id: int) -> Post:
+        query = (
+            session.query(self._model)
+            .where(self._model.id == id)
+        )
+        return query.scalar()
+    
+    def get_post_by_author(self, session: Session, author_id: int) -> Post:
+        query = (
+            session.query(self._model)
+            .where(self._model.author_id == author_id)
+        )
+        return query.scalar()
+    
+    def get_all_posts(self, session: Session) -> List[Post]:
+        query = session.query(self._model).all()
+        return query
+
+    
+    def delete_post(self, session: Session, id: int) -> bool:
+        post = self.get_post_by_id(session, id)
+        if post:
+            session.delete(post)
+            session.commit()
+            return True
+        return False
+    
+    def create_post(self, session:Session, title: str, text: str,
+                    is_published: bool, author_id: int, location_id: int | None = None,
+                    category_id: int | None = None, image: str | None = None,
+                    pup_date: datetime | None = None) -> Post:
+        post = Post(
+            title=title,
+            text=text,
+            is_published=is_published,
+            author_id=author_id,
+            location_id=location_id,
+            category_id=category_id,
+            image=image,
+            pup_date=pup_date,
+            create_at=datetime.now(),
+        )
+        session.add(post)
+        session.commit()
+        return post
+    
+    def update_post(self, session:Session, post_id: int, title: str, 
+                    text: str, is_published: bool, category_id: int | None = None,
+                    image: str | None = None) -> Post:
+        post = self.get_post_by_id(session, post_id)
+        if post:
+            post.title=title
+            post.text = text
+            post.is_published = is_published
+            post.category_id = category_id
+            post.image = image
+            session.commit()
+        return post
+
+
+
+
+
+    
