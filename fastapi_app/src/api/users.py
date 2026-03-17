d6d5baa07af34a19204a4919c464ff7aae2d670e
Popova Veronika <veronika21meow@gmail.com>
2026-03-17 21:47:43 +0400

    feat: add users api

---
+++ fastapi_app/src/api/users.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from typing import List
+
+from schemas.users import User, UserCreate, UserResponse
+
+from api.depends import (
+    get_user_by_id_use_case,
+    get_user_by_login_use_case,
+    get_user_by_email_use_case,
+    create_user_use_case,
+    delete_user_use_case,
+)
+
+users_router = APIRouter()
+
+
+@users_router.get("/profile/{user_id}", status_code=status.HTTP_200_OK, response_model=UserResponse)
+async def get_user_by_id(
+    user_id: int,
+    use_case = Depends(get_user_by_id_use_case)) -> UserResponse:
+    try:
+        user = await use_case.execute(user_id=user_id)
+        return user
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
+@users_router.get("/login/{login}", status_code=status.HTTP_200_OK, response_model=UserResponse)
+async def get_user_by_login(
+    login: str,
+    use_case = Depends(get_user_by_login_use_case)) -> UserResponse:
+    try:
+        user = await use_case.execute(login=login)
+        return user
+    except ValueError as err:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(err)
+        )
+    
+@users_router.get("/email/{email}", status_code=status.HTTP_200_OK, response_model=UserResponse)
+async def get_user_by_email(
+    email: str,
+    use_case = Depends(get_user_by_email_use_case)) -> UserResponse:
+    try:
+        user = await use_case.execute(email=email)
+        return user
+    except ValueError as err:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(err)
+        )
+
+
+@users_router.post("/register", status_code=status.HTTP_200_OK, response_model=User)
+async def create_user(
+    data: UserCreate,
+    use_case = Depends(create_user_use_case)) -> User:
+    return await use_case.execute(data)
+
+@users_router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(
+    user_id: int,
+    use_case = Depends(delete_user_use_case)):
+    try:
+        result = await use_case.execute(user_id=user_id)
+        if result:
+            return {"message": "Пользователь удален"}
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Пользователь не найден"
+        )
