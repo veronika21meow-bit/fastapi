4e6131d751edd008797681740ac0bb8342bc3c9c
Popova Veronika <veronika21meow@gmail.com>
2026-04-10 02:12:38 +0400

    fix: fixed comments schemas

--- fastapi_app/src/schemas/comments.py
+++ fastapi_app/src/schemas/comments.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class BaseComment(BaseModel):
     text: str = Field(..., description='Текст комментария')
-    post_id: int = Field(..., description='Пост')
+    post_id: int | None = Field(None, description='Пост')
     author_id: int = Field(..., description='Автор комментария')
     is_published: bool = Field(..., description='Опубликовано. Снимите галочку, чтобы скрыть публикацию.')
     create_at: datetime = Field(..., description='Добавлено')
