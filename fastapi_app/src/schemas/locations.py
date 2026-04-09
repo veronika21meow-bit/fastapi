b7655555d9b92167e91611f52aadac58ee7ba7d1
Popova Veronika <veronika21meow@gmail.com>
2026-04-09 22:04:47 +0400

    fix: fixed location schemas

--- fastapi_app/src/schemas/locations.py
+++ fastapi_app/src/schemas/locations.py
@@ -1,11 +1,14 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from datetime import datetime
 
 
 class Location(BaseModel):
-    id: int
     name: str = Field(..., description='Заголовок', max_length=256)
     is_published: bool = Field(..., description='Опубликовано. Снимите галочку, чтобы скрыть публикацию.')
     create_at: datetime = Field(..., description='Добавлено')
 
 
+class CreateLocation(BaseModel):
+    id: int
+    
+    model_config = ConfigDict(from_attributes=True)
