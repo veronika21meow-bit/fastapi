10d3b2e85f0598c14379a73b160932f1ee94a578
Popova Veronika <veronika21meow@gmail.com>
2026-04-10 01:29:04 +0400

    fix: fixed model comments

--- fastapi_app/src/infrastructure/sqlite/models/comments.py
+++ fastapi_app/src/infrastructure/sqlite/models/comments.py
@@ -11,9 +11,15 @@ class Comment(Base):
     text: Mapped[str] = mapped_column(nullable=False)
     is_published: Mapped[bool] = mapped_column(nullable=False, default=True)
     create_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
-    # Внешние ключи
-    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable=False)
-    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id", name="fk_comments_post_id", ondelete="SET NULL"), 
+        nullable=True
+    )
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", name="fk_comments_author_id"), 
+        nullable=False
+    )
+    
     # Связи
     post = relationship("Post", back_populates="comments")
     author = relationship("User", back_populates="comments")
